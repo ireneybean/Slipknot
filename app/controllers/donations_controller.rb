976787d45c58ead49e3065fc12c246eb37a0f9fa
Irene Balogun <irenebalogun@gmail.com>
2009-10-16 23:42:58 -0400

    When we go to the donation page we see expected text

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,30 @@
+class DonationsController < ApplicationController
+  # Show all records
+  def index
+  end
+  
+  # Show one record
+  def show
+  end
+  
+  # Show the form to make a new record
+  def new
+  end
+  
+  # POST action to create the record
+  def create
+  end
+  
+  # Show the form to alter exiting record
+  def edit
+  end
+  
+  # PUT action to edit the record
+  def update
+  end
+  
+  # Delete the record
+  def destroy
+  end
+  
+end
