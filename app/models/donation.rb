cb1646b03ed09486d9bcaf869168b9e543de6dca
Irene Balogun <irenebalogun@gmail.com>
2009-12-03 01:27:30 -0500

    refactored conversion to cents into a module

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,10 @@
+require 'store_amounts_as_cents'
 class Donation < ActiveRecord::Base
+  extend StoreAmountsAsCents
+  acts_as_currency :amount
   has_many :payments
   validates_numericality_of :amount_cents, :greater_than=>0
+
   
   def self.human_attribute_name(column_name)
     if (column_name == "amount_cents")
@@ -9,4 +13,5 @@ class Donation < ActiveRecord::Base
       super
     end
   end
+
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,10 @@
+require 'store_amounts_as_cents'
 class Payment < ActiveRecord::Base
-  include StoreAmountsAsCents
+  extend StoreAmountsAsCents
   belongs_to :donation
   validates_presence_of :donation
+  acts_as_currency :payment_gross
+  acts_as_currency :payment_fee
+  acts_as_currency :mc_gross
+  acts_as_currency :mc_fee
 end
