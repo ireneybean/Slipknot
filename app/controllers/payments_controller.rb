a363b51dbe81a6b7890e2d13f4b3a05003d98547
Irene Balogun <irenebalogun@gmail.com>
2009-12-13 14:27:20 -0500

    more logging in paypal_ack

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -20,7 +20,7 @@ class PaymentsController < ApplicationController
     logger.info 'Received Paypal IPN Request.  Verifying'
     params.store("cmd", "notify-verify")
     resp= Net::HTTP.post_form( URI.parse("http://sandbox.paypal.com/"), params )  #TODO: URL in constant that depends on env
-    logger.error 'Paypal verification failed for #{params.inspect}\nFailure was: resp.inspect' unless resp.body=="VERIFIED"
+    logger.error "Paypal verification failed for #{params.inspect}\nFailure was: #{resp.inspect}" unless resp.body=="VERIFIED"
     return resp.body=="VERIFIED"
     
   end
