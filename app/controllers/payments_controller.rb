6953fc564f3ffc8a661571bb05db8993ac000166
Irene Balogun <irenebalogun@gmail.com>
2009-12-15 20:16:33 -0500

    working on paypal ipn

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -20,16 +20,21 @@ class PaymentsController < ApplicationController
   
   def paypal_ack (params)
     logger.info 'Received Paypal IPN Request.  Verifying'
-    params[:cmd] = "_notify-validate"
-    params.delete(:action)
-    params.delete(:controller)
+   # params[:cmd] = "_notify-validate"
+   # params.delete(:action)
+   # params.delete(:controller)
     
-    url = URI.parse(PAYPAL_URI)
-    req = Net::HTTP::Post.new(url.path)
-    req.set_form_data(params)
-    sock = Net::HTTP.new(url.host, url.port)
-    sock.use_ssl = true
-    resp = sock.start {|http| http.request(req)}
+    query = 'cmd=_notify-validate' 
+    params.each_pair {|key, value|  query = query + '&' + key + '=' + value.first  if key != 'register/pay_pal_ipn.html/pay_pal_ipn'  } 
+    http = Net::HTTP.start("www.paypal.com", 80) 
+    resp = http.post('/cgi-bin/webscr', query) 
+    http.finish # PayPal values 
+    #url = URI.parse(PAYPAL_URI)
+    #req = Net::HTTP::Post.new(url.path)
+    #req.set_form_data(params)
+    #sock = Net::HTTP.new(url.host, url.port)
+    #sock.use_ssl = true
+    #resp = sock.start {|http| http.request(req)}
    # resp= Net::HTTP.post_form( URI.parse(PAYPAL_URI), params )  #TODO: URL in constant that depends on env
     logger.error "Paypal verification failed for #{params.inspect}\nResponse body was: #{resp.body.inspect}" unless resp.body=="VERIFIED"
     return resp.body=="VERIFIED"
