a13cf7e05a00c2cfd0417fce68a7c7a0129c6a9e
Irene Balogun <irenebalogun@gmail.com>
2009-12-16 21:43:28 -0500

    missed a change to payments_controller_spec, deleted commented out bad attempts at paypal_ipn

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -24,25 +24,8 @@ class PaymentsController < ApplicationController
     uri = URI.parse(PAYPAL_URI )
     Net::HTTP.start(uri.host, uri.port) do |request|
       status = request.post(uri.path, ipn_raw + "&cmd=_notify-validate" ).body
-   end
+    end
     
-   # params[:cmd] = "_notify-validate"
-   # params.delete(:action)
-   # params.delete(:controller)
-   # logger.info(request.body)
-   # params = request.params
-   # query = 'cmd=_notify-validate' 
-   # params.each_pair {|key, value|  query = query + '&' + key + '=' + value.first  if key != 'register/pay_pal_ipn.html/pay_pal_ipn'  } 
-   # http = Net::HTTP.start("www.paypal.com", 80) 
-   # resp = http.post('/cgi-bin/webscr', query) 
-   # http.finish # PayPal values 
-    #url = URI.parse(PAYPAL_URI)
-    #req = Net::HTTP::Post.new(url.path)
-    #req.set_form_data(params)
-    #sock = Net::HTTP.new(url.host, url.port)
-    #sock.use_ssl = true
-    #resp = sock.start {|http| http.request(req)}
-   # resp= Net::HTTP.post_form( URI.parse(PAYPAL_URI), params )  #TODO: URL in constant that depends on env
     logger.error "Paypal verification failed.\nResponse body was: #{status}" unless status=="VERIFIED"
     status=="VERIFIED"
    end
